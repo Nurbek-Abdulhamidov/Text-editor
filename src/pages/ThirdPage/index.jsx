c79993bb7f85beed8fa62edcbe94727de5f2ba8b
Nurbek-Abdulhamidov <abdulhamidnurbek@gmail.com>
2022-11-20 10:44:12 +0500

    com

--- src/pages/ThirdPage/index.jsx
+++ src/pages/ThirdPage/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Selector from "../../components/Generic/NavBottom";
 import Navbar from "../../components/Navbar";
-import Table from "../../components/Table";
+import SecondPage from "../SecondPage";
 
 const Third = () => {
   return (
@@ -9,7 +9,7 @@ const Third = () => {
       <Navbar />
       <Selector />
       <div>
-        <Table param="third" />
+        <SecondPage/>
       </div>
     </div>
   );
